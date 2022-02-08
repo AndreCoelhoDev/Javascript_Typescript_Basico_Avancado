529ad95e8f3e46834cc417fd750bdbac104ce803
AndreRicardoCoelho <andre.coelho@mega.com.br>
2022-02-08 15:24:32 -0300

    Atualizando repositório

---
+++ Seção 9-JavascriptToolingES6Modules/084-ES6ModulesImportExport/src/index.js
@@ -0,0 +1,10 @@
+import { nome, sobrenome, idade, soma, Pessoa } from './modulo1'
+//import * as MeuModulo from './modulo1'
+
+console.log(nome, sobrenome, idade);
+console.log(soma(5, 5));
+
+const p1 = new Pessoa('Luiz', 'Otavio');
+console.log(p1);
+
+//console.log(MeuModulo.p1);
